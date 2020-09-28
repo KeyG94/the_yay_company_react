49b603f24f5125883279d05a0708b7a7a3d28a33
Glenn Key <glennkey78@live.no>
2020-09-28 20:01:36 +0200

    added a navbar and tested the sass

--- the-yay-company/src/App.js
+++ the-yay-company/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import './scss/styles.scss';
 import './scss/global/global.scss';
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import Contact from './components/contact/Contact';
 import Home from './components/home/Home';
 import News from './components/news/News';
@@ -10,21 +15,22 @@ import News from './components/news/News';
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/News">News</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Contact">Contact</NavLink>
-            </li>
-          </ul>
-        </nav>
-
+      <>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">The Yay Company</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink exact to="/" className="nav-link">Home</NavLink>
+              <NavLink to="/News" className="nav-link">News</NavLink>
+              <NavLink to="/Contact" className="nav-link">Contact</NavLink>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -38,7 +44,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
-      </div>
+      </>
     </Router>
   );
 }
